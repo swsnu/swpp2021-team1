3a44139884cad5622e17e1c652d2b55dc3440c4e
Queist <queistion@gmail.com>
2021-10-10 21:17:34 +0900

    feat(LogIn,SignUp) : LogIn 및SignUp 정규표현식 구현
    LogIn 컴포넌트 구현.
    SignUp 팝업의 정규표현식 구현.
    정규표현식에서 매칭 안된 경우의 경고는 디자인의 성향이 강한 것 같아 남겨둠.
    백엔드가 구현되면 에러 처리에 대한 부분 추가해야함.
    SignUp, RestorePassword 등의 팝업 창을 LogIn에서 modal로 띄우는 것에
    대한 구현 필요.
    부트스트랩을 이용하면 간단하다고 했기에 따로 남겨둠.

--- src/container/login/LogIn.tsx
+++ src/container/login/LogIn.tsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {StoreDispatch, StoreState} from "../../store/Store";
+import {DummyUser, User} from "../../Interfaces";
+import {Redirect} from "react-router-dom";
+import * as actionCreator from "../../store/slice/UserSlice"
 
 interface LogInProps {
 
 }
 
 export default function LogIn(props : LogInProps) {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const dispatch = useDispatch<StoreDispatch>();
+    const [account, isLoading, hasError] =
+        useSelector<StoreState, [User|null, DummyUser|null, DummyUser[], boolean, boolean]>(state =>
+            [state.users.account, state.users.currentUser, state.users.friends, state.users.isLoading, state.users.hasError]);
 
+    function onLogIn() {
+        dispatch(actionCreator.logIn({email, password}));
+    }
+
+    //error 처리 필요
+    if (isLoading && !hasError) return null;
     return (
         <div>
-
+            {account && <Redirect to={`/main/${account.nickName}`}/>}
+            <div>Email : <input value={email} type="text" onChange={event => setEmail(event.target.value)}/></div>
+            <div>Password : <input value={password} type="text" onChange={event => setPassword(event.target.value)}/></div>
+            <button onClick={onLogIn} disabled={email === "" || password === ""}>Log In</button>
+            <button>Sign Up!</button>{/*TODO : Modal SignUp.tsx with onClick*/}
+            <button>Forget Password?</button>{/*TODO : Modal RestorePassword.tsx with onClick*/}
         </div>
     )
 
--- src/container/login/popup/SignUp.tsx
+++ src/container/login/popup/SignUp.tsx
@@ -13,13 +13,37 @@ export default function SignUp(props : SignUpProps) {
     const [nickName, setNickName] = useState<string>("");
     const [name, setName] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [canUse, setCanUse] = useState<boolean|null|undefined>(undefined);
+    //해당 닉네임을 이용할 수 있는지 확인하는 state
     const dispatch = useDispatch<StoreDispatch>();
     const [isLoading, hasError] = useSelector<StoreState, [boolean, boolean]>(state =>
         [state.users.isLoading, state.users.hasError]);
 
+    function onCheck() {
+        setCanUse(null);
+        /*TODO : request api server to check duplication*/
+    }
 
     function onSignUp() {
         /*정규표현식으로 signup 형식 지정(hw2 참조)*/
+        let flag = true;
+        if (!/^[^.@\s]+@[A-Za-z\d.]+$/.test(email)) {
+            //TODO
+            flag = false;
+        }
+        if (!/^[a-zA-Z\d][_]+$/.test(nickName)) {
+            //TODO
+            flag = false;
+        }
+        if (!/^[a-zA-Z]+$/.test(name)) {
+            //TODO
+            flag = false;
+        }
+        if (!/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password)) {
+            //TODO
+            flag = false;
+        }
+        if (!flag || canUse !== true) return;
         dispatch(actionCreators.signUp({email : email, nickName : nickName, name : name, password : password, profilePicture : 'default'}));
     }
 
@@ -29,6 +53,10 @@ export default function SignUp(props : SignUpProps) {
         <div>
             <div>Email :  <input value={email} type={"text"} onChange={event => setEmail(event.target.value)}/></div>
             <div>NickName :  <input value={nickName} type={"text"} onChange={event => setNickName(event.target.value)}/></div>
+            <button onClick={onCheck}>Check Duplicated NickName</button>
+            <div>{canUse === true ? 'Acceptable NickName' :
+                    canUse === false ? 'Duplicated NickName' :
+                    canUse === null ? 'Loading...' : 'Duplication Check Please'}</div>
             <div>Name :  <input value={name} type={"text"} onChange={event => setName(event.target.value)}/></div>
             <div>Password :  <input value={password} type={"text"} onChange={event => setPassword(event.target.value)}/></div>
             <button onClick={onSignUp}>Confirm</button>
