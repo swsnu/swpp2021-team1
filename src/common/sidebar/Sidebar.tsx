54d16709e1bc1a26ff58eef611571b9bd3511a31
lemonshushu <lemonshushuu@gmail.com>
2021-10-28 20:12:19 +0900

    refactor(sidebar) : Refactor Sidebar menu ids

--- src/common/sidebar/Sidebar.tsx
+++ src/common/sidebar/Sidebar.tsx
@@ -17,27 +17,27 @@ const Sidebar = (props: SidebarProps) => {
       {/* <hr /> */}
         <ul className="nav flex-column mb-auto mt-2">
           <li className="nav-item">
-            <a href="#" className="nav-link link-dark fs-5" aria-current="page">
+            <a id="posts-menu" href="#" className="nav-link link-dark fs-5" aria-current="page">
               <FontAwesomeIcon className="me-3" icon={faPencilAlt} color="#f69d72" />
               Posts
             </a>
 
-            <a href="#" className="nav-link link-dark fs-5">
+            <a id="repositories-menu" href="#" className="nav-link link-dark fs-5">
               <FontAwesomeIcon className="me-3" icon={faBook} color="#f69d72" />
               Repositories
             </a>
-            <a href="#" className="nav-link link-dark mb-4 fs-5">
+            <a id="explore-menu" href="#" className="nav-link link-dark mb-4 fs-5">
               <FontAwesomeIcon className="me-3" icon={faCompass} color="#f69d72" />
               Explore
             </a>
-            <a href="#" className="nav-link link-dark fs-5">
+            <a id="noticelist-menu" href="#" className="nav-link link-dark fs-5">
               <FontAwesomeIcon className="me-3" icon={faBell} color="#f69d72" />Notifications
             </a>
           </li>
         </ul>
         <hr />
           <Dropdown>
-            <Dropdown.Toggle id="dropdown-toggle" className="d-flex align-items-center link-dark text-decoration-none" data-bs-toggle="dropdown">
+            <Dropdown.Toggle id="settings-menu" className="d-flex align-items-center link-dark text-decoration-none" data-bs-toggle="dropdown">
               <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2" />
               <strong>John Doe</strong>
             </Dropdown.Toggle>
