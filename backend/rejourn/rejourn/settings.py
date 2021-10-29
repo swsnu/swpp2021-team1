6b358267dc019371d0509b54a861c86d237badd6
youna1222 <youna122221@naver.com>
2021-10-29 20:17:33 +0900

    add media path to settings

--- backend/rejourn/rejourn/settings.py
+++ backend/rejourn/rejourn/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'project.User'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
