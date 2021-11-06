9816654d9481a1c5a03017f51072ff509aeff2aa
lemonshushu <lemonshushuu@gmail.com>
2021-11-06 22:10:16 +0900

    fix(testing): comment out mock server setup code for testing

--- frontend/src/setupTests.tsx
+++ frontend/src/setupTests.tsx
@@ -11,6 +11,7 @@ Enzyme.configure({
     disableLifecycleMethods: true,
 });
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers);
-afterAll(() => server.close());
+// mock server를 쓰고 싶다면 이 밑 3줄 주석을 해제하시오!
+// beforeAll(() => server.listen());
+// afterEach(() => server.resetHandlers);
+// afterAll(() => server.close());
