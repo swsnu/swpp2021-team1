0d5fa5f14fcfea01c5467ee2a3ff1740df741e87
lemonshushu <lemonshushuu@gmail.com>
2021-11-09 19:33:55 +0900

    refactor(comments) : refactor comment component

--- frontend/src/common/Interfaces.tsx
+++ frontend/src/common/Interfaces.tsx
@@ -108,7 +108,7 @@ interface IDiscussion {
 interface IComment {
     comment_id : number;
     parent_id : number; // discussion_id / post_id parent_id로 바꿔서 줘야함
-    author? : string;
+    author? : IUser;
     text : string;
     post_time? : string;
 }
--- frontend/src/features/comments/Comment.tsx
+++ frontend/src/features/comments/Comment.tsx
@@ -12,92 +12,89 @@ interface CommentProps {
 }
 
 const Comment = (props: CommentProps) => {
-    const dispatch = useAppDispatch();
+    // const dispatch = useAppDispatch();
     const {
         comment, isEditable, edit, del,
     } = props;
-    const [loading, setLoading] = useState<"idle" | "pending" | "succeeded" | "failed">("idle");
-    const [author, setAuthor] = useState<IUser | null>(null);
+    // const [loading, setLoading] = useState<"idle" | "pending" | "succeeded" | "failed">("idle");
+    const { author } = comment;
+    // const [author, setAuthor] = useState<IUser | null>(null);
 
-    useEffect(() => {
-        const getAuthorInfo = async (username: string) => {
-            setLoading("pending");
-            try {
-                const user = await getUser(username);
-                setAuthor(user);
-                setLoading("succeeded");
-            }
-            catch (e) {
-                setLoading("failed");
-            }
-        };
-        if (loading === "idle" && comment.author) {
-            getAuthorInfo(comment.author);
-        }
-    }, [dispatch]);
+    // useEffect(() => {
+    //     const getAuthorInfo = async (username: string) => {
+    //         setLoading("pending");
+    //         try {
+    //             const user = await getUser(username);
+    //             setAuthor(user);
+    //             setLoading("succeeded");
+    //         }
+    //         catch (e) {
+    //             setLoading("failed");
+    //         }
+    //     };
+    //     if (loading === "idle" && comment.author) {
+    //         getAuthorInfo(comment.author);
+    //     }
+    // }, [dispatch]);
 
-    if (loading === "succeeded") {
-        return (
-            <>
-                <div className="bg-white p-2">
-                    <div className="d-flex flex-row user-info">
-                        <img
-                            className="rounded-circle"
-                            src={author?.profile_picture}
-                            width="40"
-                            alt="author profile"
-                        />
-                        <div className="d-flex flex-column justify-content-start ml-2">
-                            <span className="d-block name">
-                                @
-                                {author?.username}
-                            </span>
-                            <span className="date text-black-50">{comment.post_time as string}</span>
-                        </div>
-                    </div>
-                    <div className="mt-2">
-                        <p className="comment-text">{comment.text}</p>
+    return (
+        <>
+            <div className="bg-white p-2">
+                <div className="d-flex flex-row user-info">
+                    <img
+                        className="rounded-circle"
+                        src={author?.profile_picture}
+                        width="40"
+                        alt="author profile"
+                    />
+                    <div className="d-flex flex-column justify-content-start ml-2">
+                        <span className="d-block name">
+                            @
+                            {author?.username}
+                        </span>
+                        <span className="date text-black-50">{comment.post_time as string}</span>
                     </div>
                 </div>
-                <div className="bg-white">
-                    <div className="d-flex flex-row fs-12">
-                        <div className="like p-2 cursor">
+                <div className="mt-2">
+                    <p className="comment-text">{comment.text}</p>
+                </div>
+            </div>
+            <div className="bg-white">
+                <div className="d-flex flex-row fs-12">
+                    {/* <div className="like p-2 cursor">
                             <i className="fa fa-thumbs-o-up" />
                             <span className="ml-1">Like</span>
-                        </div>
-                        {
-                            isEditable && (
-                                <>
-                                    <div
-                                        className="like p-2 cursor"
-                                        role="button"
-                                        tabIndex={0}
-                                        onClick={edit}
-                                        onKeyDown={edit}
-                                    >
-                                        <i className="fa fa-edit-o" />
-                                        <span className="ml-1">Edit</span>
-                                    </div>
-                                    <div
-                                        className="like p-2 cursor"
-                                        role="button"
-                                        tabIndex={0}
-                                        onClick={del}
-                                        onKeyDown={del}
-                                    >
-                                        <i className="fa fa-trash" />
-                                        <span className="ml-1">Delete</span>
-                                    </div>
-                                </>
-                            )
-                        }
-                    </div>
+                        </div> */}
+                    {
+                        isEditable && (
+                            <>
+                                <div
+                                    className="like p-2 cursor"
+                                    role="button"
+                                    tabIndex={0}
+                                    onClick={edit}
+                                    onKeyDown={edit}
+                                >
+                                    <i className="fa fa-edit-o" />
+                                    <span className="ml-1">Edit</span>
+                                </div>
+                                <div
+                                    className="like p-2 cursor"
+                                    role="button"
+                                    tabIndex={0}
+                                    onClick={del}
+                                    onKeyDown={del}
+                                >
+                                    <i className="fa fa-trash" />
+                                    <span className="ml-1">Delete</span>
+                                </div>
+                            </>
+                        )
+                    }
                 </div>
-            </>
-        );
-    }
-    if (loading === "failed") return <>Failed to load comment!</>;
-    return <></>;
+            </div>
+        </>
+    );
 };
 
 export default Comment;
