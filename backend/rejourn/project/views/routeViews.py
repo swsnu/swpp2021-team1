eda2fcf59384be2a37855a6d86708fea0bac7362
youna1222 <youna122221@naver.com>
2021-11-15 17:10:49 +0900

    add route urls

--- backend/rejourn/project/views/routeViews.py
+++ backend/rejourn/project/views/routeViews.py
@@ -12,3 +12,12 @@ from project.httpResponse import *
 from project.utils import have_common_user
 from project.enum import Scope
 
+@require_http_methods(["POST", "GET"])
+@ensure_csrf_cookie
+def routeSearch(request, repo_id):
+    if request.method == "GET":
+        return HttpResponseSuccessGet()
+
+@require_http_methods(["PUT", "DELETE", "GET"])
+@ensure_csrf_cookie
+def route(reques)
